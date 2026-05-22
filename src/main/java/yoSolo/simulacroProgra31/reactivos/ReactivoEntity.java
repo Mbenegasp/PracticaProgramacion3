acfc11b12ac51a8eba41e06b55e0b90efe64087b
Mateo <mbenegas2001@gmail.com>
2026-05-21 23:07:00 -0300

    ReactivoEntity creada

---
+++ src/main/java/yoSolo/simulacroProgra31/reactivos/ReactivoEntity.java
@@ -0,0 +1,28 @@
+package yoSolo.simulacroProgra31.reactivos;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "reactivos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+
+public class ReactivoEntity {
+    @EqualsAndHashCode.Include
+    @Column(name = "reactivo_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "nivel_peligro", nullable = false)
+    private Integer nivelPeligro;
+    @Column(name = "es_precursor_quimico", nullable = false)
+    private Boolean esPrecursorQuimico;
+    @Column(name = "activo", nullable = false)
+    private Boolean activo;
+}
